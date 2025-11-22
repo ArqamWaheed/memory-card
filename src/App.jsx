0cc10d9665e3bb5f2acf4c87c6df5809bb3459db
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-22 23:05:02 +0500

    Added fetch and stff

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import './App.css'
-
+import { useState, useEffect } from 'react';
 function App() {
+  const [src, setSrc] = useState(null);
+  useEffect(() => {
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=9DJ3r9jvTt5JDGWaee7k3T1Hg2SO8xtk&q=OnePieceLuffy", {
+      mode: "cors",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setSrc(data.data[0].images.original_still.url);
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+  }, []);
+
 
   return (
     <>
+    <img src={src} alt="test" />
     </>
   )
 }
 
-export default App
+export default App;
